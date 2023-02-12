54fab963b1a9a5eeb9321df58508672b7c5c2e85
2022PC <dieltidielti@gmail.com>
2023-02-12 23:18:48 +0100

    blyyaaa

--- app/head.tsx
+++ app/head.tsx
@@ -2,6 +2,8 @@ export default function Head() {
   return (
     <>
       <link rel="icon" href="/images/favicon.png" />
+      <meta content="charset=UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
       <title>Електронний кабінет МСП</title>
     </>
   )
